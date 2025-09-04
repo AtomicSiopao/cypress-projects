1dcb772769c40fdcdd9605d1c60303f529d5e0ce
AtomicSiopao <jmsmrco@gmail.com>
2025-09-05 00:14:37 +0800

    Added checkboxes to exercise sciprt. Updated README.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # SETUP
 ## Download dependencies
- - NodeJS
- - PNPM
+ - [NodeJS](https://nodejs.org/en)
+ - pnpm
  
+## Install pnpm using npm and PowerShell
+ - `npm install -g pnpm`
+
 ## Install Cypress.io via pnpm
  - `pnpm init`
  - `pnpm add --save-dev cypress`
--- cypress/e2e/customtests/commitQualityPractice.cy.js
+++ cypress/e2e/customtests/commitQualityPractice.cy.js
@@ -21,12 +21,21 @@ describe('General Components Practice', () => {
         cy.contains('.radio-buttons-container > .component-container > p','option2 clicked').should('be.visible');
     });
 
-    it('Select Options from dropdow', () => {
+    it('Select Options from dropdown', () => {
         cy.get('.container-text').contains('General Components').click();
         cy.get('select').select('Option 1').contains('select','Option 1').should('be.visible');
         cy.get('select').select('Option 2').contains('select','Option 2').should('be.visible');
         cy.get('select').select('Option 3').contains('select','Option 3').should('be.visible');
+    });
 
+    it('Toggle checkboxes', () => {
+        cy.get('.container-text').contains('General Components').click();
+        cy.get('[data-testid="checkbox1"]').click();
+        cy.contains('.checkbox-container.container-outline > :nth-child(2) > :nth-child(1) > p', 'Checkbox 1 checked').should('be.visible');
+        cy.get('[data-testid="checkbox2"]').click();
+        cy.contains('.checkbox-container.container-outline > :nth-child(2) > :nth-child(2) > p', 'Checkbox 2 checked').should('be.visible');
+        cy.get('[data-testid="checkbox3"]').click();
+        cy.contains('.checkbox-container.container-outline > :nth-child(2) > :nth-child(3) > p', 'Checkbox 3 checked').should('be.visible');
     });
     
 });
