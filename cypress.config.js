9ef4a6b5c4f955817d0df13c0717171b7054344e
AtomicSiopao <jmsmrco@gmail.com>
2025-09-17 01:33:42 +0800

    bugfix: browserstack config

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,9 @@
 const { defineConfig } = require("cypress");
-require("dotenv").config();
 module.exports = defineConfig({
   projectId: "psrfe1",
   e2e: {
     "chromeWebSecurity": false,
     specPattern: "cypress/e2e/customtests/**/*.cy.js",
-    setupNodeEvents(on, config) {
-      config.env.BROWSERSTACK_USERNAME = process.env.BROWSERSTACK_USERNAME;
-      config.env.BROWSERSTACK_ACCESS_KEY = process.env.BROWSERSTACK_ACCESS_KEY;
-      return config;
-    },
     viewportWidth: 1280,
     viewportHeight: 720,
   },
